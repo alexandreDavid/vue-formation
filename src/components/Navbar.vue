9e305e4cdaa9c5d0b8eabfbc74bf1d433cbf2219
Alexandre David <alexandre.David42@gmail.com>
2020-12-06 15:00:00 -0300

    Navbar in component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark navbar-expand">
+    <a class="navbar-brand" href="/">
+      <img class="logo" alt="Vue logo" src="@/assets/logo.png">
+      SeekMovie
+    </a>
+    <div class="collapse navbar-collapse justify-content-between">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Accueil</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">A propos</router-link>
+        </li>
+      </ul>
+      <form class="form-inline" @submit.prevent="search(valueToSearchInput)">
+        <input class="form-control mr-sm-2"
+          v-model="valueToSearchInput"
+          type="search"
+          placeholder="Saisissez un titre"
+          aria-label="Saisissez un titre">
+        <button class="btn btn-success my-2 my-sm-0" type="submit">
+          Rechercher
+        </button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+import SearchHistory from '@/store/search-history'
+
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      valueToSearchInput: '',
+      searchedValue: ''
+    }
+  },
+  methods: {
+    search (search) {
+      this.searchedValue = search
+      SearchHistory.add(search)
+      this.$router.push({ path: 'result', query: { search } })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.navbar .logo {
+  width: 30px;
+}
+</style>
