fc5a4967f722b55f97d0177f5e36070bc62283b6
Alexandre David <alexandre.David42@gmail.com>
2020-12-07 15:36:05 +0100

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <Navbar/>
   <router-view/>
+  <Footer/>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'App',
-  components: { Navbar }
+  components: { Navbar, Footer }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="bg-dark text-white py-3">
+    <div class="container">
+      <div class="d-flex justify-content-center">
+        <div class="my-2 px-2 border-right">Formation Vue.js</div>
+        <div class="my-2 px-2 border-right">Du 7 au 9 Décembre 2020</div>
+        <a class="btn btn-link" href="https://v3.vuejs.org/">Accéder à la documentation</a>
+      </div>
+    </div>
+  </footer>
+</template>
