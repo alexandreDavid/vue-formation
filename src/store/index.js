f6edf9fe5087bf44640fff2eade3b42fc75e983d
Alexandre David <alexandre.David42@gmail.com>
2020-12-09 15:41:37 +0100

    Utilisation du localstorage dans le store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
 import { createStore } from 'vuex'
+const favourites = (localStorage.favourites && localStorage.favourites.split(',')) || []
 
 export default createStore({
   state: {
-    favourites: []
+    favourites
   },
   mutations: {
     addFav (state, fav) {
       state.favourites.push(fav)
+      localStorage.favourites = state.favourites
     },
     removeFav (state, fav) {
       state.favourites = state.favourites.filter(f => f !== fav)
+      localStorage.favourites = state.favourites
     }
   },
   actions: {
